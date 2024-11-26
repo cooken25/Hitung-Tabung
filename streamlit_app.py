7767c9380a29cc79d968e79df8b31dc630d733d7
cooken25 <andiwahyudin321@gmail.com>
2024-11-26 16:32:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
-   import streamlit as st 
-   import math
+import streamlit as st 
+import math
 
 st.title(" Menghitung Tabung  :red[Valume tabung] :smile:")
 r = st.number_input("masukan jari-jari :",0)
